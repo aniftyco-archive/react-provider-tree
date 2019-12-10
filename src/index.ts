85c2705a536b021eee13c2e075cd7f2f81d6fc74
Josh Manders <josh@joshmanders.com>
2019-12-10 17:57:03 -0600

    fix types

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
-import { Fragment, createElement } from 'react';
+import { createElement, ComponentElement } from 'react';
 
 export type Provider = [any, Record<string, any>];
 
-const createProviderTree = (providers: Provider[], index: number = 0, children: any) => {
+const createProviderTree = (providers: Provider[], index: number = 0, children: any): ComponentElement<any, any> => {
   const [Provider, props = {}] = providers[index];
 
   if (index === providers.length - 1) {
     return createElement(Provider, { ...props, children });
   }
 
-  return createElement(Provider, props, createProviderTree(providers, ++index, children));
+  return (createElement(
+    Provider,
+    props,
+    createProviderTree(providers, ++index, children)
+  ) as unknown) as ComponentElement<any, any>;
 };
 
 export const createProviderTreeFromList = (...providers: Provider[]) => {
