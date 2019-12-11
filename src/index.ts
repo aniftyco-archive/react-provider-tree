b75089bec2adb934f91b366e1dd6fc6a82c9b5d9
Josh Manders <josh@joshmanders.com>
2019-12-10 18:12:28 -0600

    shift off provider  instead of using index

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,21 @@ import { createElement, ComponentElement } from 'react';
 
 export type Provider = [any, Record<string, any>];
 
-const createProviderTree = (providers: Provider[], index: number = 0, children: any): ComponentElement<any, any> => {
-  const [Provider, props = {}] = providers[index];
+const createProviderTree = (providers: Provider[], children: any): ComponentElement<any, any> => {
+  const [Provider, props = {}] = providers.shift() || [];
 
-  if (index === providers.length - 1) {
+  if (providers.length === 0) {
     return createElement(Provider, { ...props, children });
   }
 
-  return (createElement(
-    Provider,
-    props,
-    createProviderTree(providers, ++index, children)
-  ) as unknown) as ComponentElement<any, any>;
+  return (createElement(Provider, props, createProviderTree(providers, children)) as unknown) as ComponentElement<
+    any,
+    any
+  >;
 };
 
 export const createProviderTreeFromList = (...providers: Provider[]) => {
   return ({ children }: { children?: any }) => {
-    return createProviderTree(providers, 0, children);
+    return createProviderTree(providers, children);
   };
 };
